90422392657e129e1adbd59db2a2d6123e1afafe
graceywambui <grace.wambui@student.moringaschool.com>
2022-12-05 19:20:05 +0300

    changed and added and removed HTML elements via DOM through Javascript

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+//someElement.removeChild(someChildElement);
+document.getElementById('main').remove();
+
+//document.body.append(element);
+//const ul = document.createElement("ul");
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
